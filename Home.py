308530c057ff4edb721c60e98d42b45ebffae56d
Beck Orion <beckorion@gmail.com>
2024-05-07 13:00:56 -0700

    first commit

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config("Homepage", "🏠", layout='wide')
+
+st.title("2024 Computer Science Final Project - Graphing!")
+
+st.markdown("*Description of what the students did...*")
+
+st.markdown("**<- Click a name of the lefthand side to see their graph**")
